4f0994ee30fba413c06dd6b3d48bdb70b0bd5caf
Ben H <ben.hollamby@arkahna.io>
2024-09-01 16:55:15 +1200

    pass vars from gha to tf

---
+++ vars.tf
@@ -0,0 +1,14 @@
+variable "subscription_id" {
+  description = "The subscription ID for Azure"
+  type        = string
+}
+
+variable "client_id" {
+  description = "The client ID for the Azure Service Principal"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "The tenant ID for Azure"
+  type        = string
+}
